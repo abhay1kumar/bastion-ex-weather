84077499e76c233eded9f993a711f4bd00ea1265
Abhay Kumar <abhay4waytechnologies@gmail.com>
2023-06-26 08:28:32 +0530

    complete

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,15 +1,15 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback, memo } from "react";
 import "./WeatherForecast.css";
-
-  
 
 const Weather = () => {
   const [weatherData, setWeatherData] = useState(null);
-  const getData = async () => {
+
+  const getData = useCallback(async () => {
     const config = {
       method: "get",
-      url: "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m",
+      url:
+        "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m",
       headers: {
         "Content-Type": "application/json",
       },
@@ -21,20 +21,22 @@ const Weather = () => {
     } catch (error) {
       console.log(error);
     }
-  };
+  }, []);
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [getData]);
 
-  const splitDateTime = (dateTime) => {
-    const [date, time] = dateTime.split('T');
+  const splitDateTime = useCallback((dateTime) => {
+    const [date, time] = dateTime.split("T");
     return { date, time };
-  };
+  }, []);
 
   if (!weatherData) {
     return <div>Loading...</div>;
   }
+
+
 
   return (
     <>
@@ -46,7 +48,9 @@ const Weather = () => {
             width="100px"
             height="100px"
           />
-          <p style={{color:"#fff", fontSize:"16px"}}>{weatherData.current_weather.time.split("T")[0]}</p>
+          <p style={{color:"#fff", fontSize:"16px"}}>
+            {weatherData.current_weather.time.split("T")[0]}
+          </p>
         </div>
         <div className="WeaterInformation">
           <div className="weatherCard">
@@ -56,7 +60,9 @@ const Weather = () => {
               width="100px"
               height="100px"
             />
-            <p style={{color:"#fff", fontSize:"16px"}}>{weatherData.current_weather.temperature}°C</p>
+            <p style={{color:"#fff", fontSize:"16px"}}>
+              {weatherData.current_weather.temperature}°C
+            </p>
             <p style={{color:"#fff", fontSize:"16px"}}>Temperature</p>
           </div>
 
@@ -67,46 +73,51 @@ const Weather = () => {
               width="100px"
               height="100px"
             />
-            <p style={{color:"#fff", fontSize:"16px"}}>{weatherData.current_weather.windspeed} km/h</p>
+            <p style={{color:"#fff", fontSize:"16px"}}>
+              {weatherData.current_weather.windspeed} km/h
+            </p>
             <p style={{color:"#fff", fontSize:"16px"}}>Wind Speed</p>
           </div>
         </div>
-      
-    {/* Table */}
 
-
-    <div>
-      <h2 style={{color:"#fff"}}>Hourly Weather</h2>
-      <table style={{ width: '100%', borderCollapse: 'collapse' }}>
-        <thead>
-          <tr>
-            <th style={{border:"1px solid #ddd" , color:"#fff"}}>Date</th>
-            <th style={{border:"1px solid #ddd" , color:"#fff"}}>Time</th>
-            <th style={{border:"1px solid #ddd" , color:"#fff"}}>Temperature (°C)</th>
-            <th style={{border:"1px solid #ddd" , color:"#fff"}}>Relative Humidity (%)</th>
-            <th style={{border:"1px solid #ddd" , color:"#fff"}}>Wind Speed (m/s)</th>
-          </tr>
-        </thead>
-        <tbody>
-        {weatherData.hourly.time.map((time, index) => {
-            const { date, time: hour } = splitDateTime(time);
-            return (
-              <tr key={time}>
-                <td className="cellStyles">{date}</td>
-                <td className="cellStyles">{hour}</td>
-                <td className="cellStyles">{weatherData.hourly.temperature_2m[index]} °C</td>
-                <td className="cellStyles">{weatherData.hourly.relativehumidity_2m[index]} %</td>
-                <td className="cellStyles">{weatherData.hourly.windspeed_10m[index]} m/s</td>
+        {/* Table */}
+        <div>
+          <h2 style={{ color: "#fff" }}>Hourly Weather</h2>
+          <table style={{ width: "100%", borderCollapse: "collapse" }}>
+            <thead>
+              <tr>
+                <th style={{border:"1px solid #ddd", color:"#fff"}}>Date</th>
+                <th style={{border:"1px solid #ddd", color:"#fff"}}>Time</th>
+                <th style={{border:"1px solid #ddd", color:"#fff"}}>Temperature (°C)</th>
+                <th style={{border:"1px solid #ddd", color:"#fff"}}>Relative Humidity (%)</th>
+                <th style={{border:"1px solid #ddd", color:"#fff"}}>Wind Speed (m/s)</th>
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-    
+            </thead>
+            <tbody>
+              {weatherData.hourly.time.map((time, index) => {
+                const { date, time: hour } = splitDateTime(time);
+                return (
+                  <tr key={time}>
+                    <td className="cellStyles">{date}</td>
+                    <td className="cellStyles">{hour}</td>
+                    <td className="cellStyles">
+                      {weatherData.hourly.temperature_2m[index]} °C
+                    </td>
+                    <td className="cellStyles">
+                      {weatherData.hourly.relativehumidity_2m[index]} %
+                    </td>
+                    <td className="cellStyles">
+                      {weatherData.hourly.windspeed_10m[index]} m/s
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
 };
 
-export default Weather;
+export default memo(Weather);
